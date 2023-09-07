85d9bb0c5982ad76e35820c277225c7aaa9c0363
Agu <aguakaolisa2004@gmail.com>
2023-09-07 12:58:48 -0700

    ss

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point of the program
  *
- * Description: This function prints a specified text with proper grammar
- *              using the printf function.
+ * Description: This function prints a specified text with proper grammar using the printf function.
+ *
  *
  * Return: 0 (Success)
  */
+
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
 }
