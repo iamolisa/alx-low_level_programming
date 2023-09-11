9b1d4b831122b0bcf6e2ce7b90726e03651e214f
Agu <aguakaolisa2004@gmail.com>
2023-09-11 10:41:59 -0700

    sd

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all single-digit numbers in base 10
+ *              starting from 0, followed by a new line, using putchar.
+ *
+ * Return: Always 0 (Success)
+**/
+int main(void)
+{
+	putchar('0' + 0); // Convert and print 0
+	putchar('0' + 1); // Convert and print 1
+	putchar('0' + 2); // Convert and print 2
+	putchar('0' + 3); // Convert and print 3
+	putchar('0' + 4); // Convert and print 4
+	putchar('0' + 5); // Convert and print 5
+	putchar('0' + 6); // Convert and print 6
+	putchar('0' + 7); // Convert and print 7
+	putchar('0' + 8); // Convert and print 8
+	putchar('0' + 9); // Convert and print 9
+
+	putchar('\n');
+
+	return (0);
+}
