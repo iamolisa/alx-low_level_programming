7741d926cba2ffebcb97418f055e2104e310f30f
Agu <aguakaolisa2004@gmail.com>
2023-09-13 13:57:07 -0700

    sa

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds a and b and returns the result
+ * @a: a number to be added to b
+ * @b: a number to be added to a
+ * Return: sumation of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
