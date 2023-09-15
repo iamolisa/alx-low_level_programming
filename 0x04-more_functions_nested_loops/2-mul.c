459b85bd5479e1299413d3bb33a62923a873cffa
Agu <aguakaolisa2004@gmail.com>
2023-09-15 14:33:50 -0700

    de

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - prototype function for int
+ *
+ * @a: used to declare 1st integer
+ *
+ * @b: used to declare 2nd integer
+ *
+ * Return: returns multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
