0e6ce28c86151d864230b05d0fc976c69a6b94f9
Agu <aguakaolisa2004@gmail.com>
2023-10-13 12:45:30 -0700

    funx=ctin that executes a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,19 +1,20 @@
-#include <stddef.h>
-
+#include "function_pointers.h"
 /**
- * array_iterator - Execute a function on each element of an array
- * @array: Pointer to the array
- * @size: Size of the array
- * @action: Pointer to the function to execute on each element
- */
-void array_iterator(int *array, size_t size, void (*action)(int)) {
-    if (array == NULL || action == NULL)
-        return;
-
-    size_t i = 0;
-
-    while (i < size) {
-        action(array[i]);
-        i++;
-    }
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: array to be used
+ * @size: size of array
+ * @action: pointer to func
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && action != NULL)
+	{
+		while (size != 0)
+		{
+			action(*array);
+			array++;
+			size--;
+		}
+	}
 }
